cc36506965f1e8f69479be4a95854115ee30da52
ki-chi <k.brilliant@gmail.com>
2020-03-18 00:57:43 +0900

    Add the empty tests

---
+++ test/julia-formatter-test.el
@@ -0,0 +1,16 @@
+(require 'ert)
+(require 'julia-formatter)
+
+(eval-when-require (require 'json))
+
+(ert-deftest jf-test-format ()
+  "Test of `julia-format`, the core of the packages' functions"
+  )
+
+(ert-deftest jf-test-format-region ()
+  "Test of `julia-format-region`"
+  )
+
+(ert-deftest jf-test-format-buffer ()
+  "Test of `julia-format-buffer`"
+  )
